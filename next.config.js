07050c29c23acb9586442dde305cb4d86882cdd1
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-06-14 01:48:31 +0300

    Fix commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withImages = require('next-images')
+module.exports = withImages()
+
+const path = require('path')
+module.exports = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+}
